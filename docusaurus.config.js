dd9e643f78c65f472f7984d56d7314b5f5f67e0b
benank <terkolskie@gmail.com>
2021-08-30 12:25:10 -0700

    More setup for API

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,85 +3,77 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Luaxe',
-  tagline: 'Create wonderous gamemodes for Valheim with Lua',
-  url: 'https://luaxe.dev',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Paradigm-MP', // Usually your GitHub org/user name.
-  projectName: 'Luaxe-docs', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'Luaxe',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'main',
-          position: 'left',
-          label: 'API Reference',
-        },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/Paradigm-MP/Luaxe',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
+    title: 'Luaxe',
+    tagline: 'Create wonderous gamemodes for Valheim with Lua',
+    url: 'https://luaxe.dev',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'Paradigm-MP', // Usually your GitHub org/user name.
+    projectName: 'Luaxe-docs', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Luaxe',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
             },
-          ],
+            items: [
+                {
+                    type: 'doc',
+                    docId: 'main',
+                    position: 'left',
+                    label: 'API Reference',
+                },
+                // {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://github.com/Paradigm-MP/Luaxe',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    href: 'http://discord.paradigm.mp',
+                    label: 'Discord',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          title: 'Community',
-          items: [
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+        algolia: {
+            apiKey: 'YOUR_API_KEY',
+            indexName: 'YOUR_INDEX_NAME',
+            // Optional: see doc section below      
+            contextualSearch: true,
+            // Optional: see doc section below      
+            appId: 'YOUR_APP_ID',
+            // Optional: Algolia search parameters      
+            searchParameters: {},
+            //... other Algolia params    
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Discord',
-              href: 'http://discord.paradigm.mp',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/Paradigm-MP/Luaxe-docs/edit/main/website/',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/Paradigm-MP/Luaxe-docs/edit/main/website/blog/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Paradigm. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/Paradigm-MP/Luaxe-docs/edit/main/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/Paradigm-MP/Luaxe-docs/edit/main/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
